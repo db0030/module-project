5047cf3b54e432a3fb569ef537c105df07de995d
db0030 <db0030@zehntech.com>
2016-10-12 17:59:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+
+<script type="text/javascript" >
+function store()
+			{
+				var nam = document.getElementById("name").value;
+				var des = document.getElementById("description").value;
+				var prio = document.getElementById("priority").value;
+				var dat = document.getElementById("date").value;
+				var stat = document.getElementById("status").value;
+
+				document.write("Name:  " + nam + "<br>");
+				document.write("Description: " + des + "<br>");
+				document.write("Priority: " + prio + "<br>");
+				document.write("Date: " + dat + "<br>");
+				document.write("Status: " + stat + "<br>");
+				return false;
+
+			}	
+</script>
+
+
+
+
